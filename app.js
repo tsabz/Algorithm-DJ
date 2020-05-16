19a2bf3dd73f3179b97542ec51349e5cd4b7e852
Tonia Saba <teesabz@Mac-mini.local>
2020-05-16 15:39:58 -0400

    spotify authentication

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('hello');
 // Redirect user to authorize with Spotify
-console.log('redirecting');
-window.location.href = "https://tsabz.github.io/Algorthim-DJ-Part-1-/?code=AQAi8OmfGYck8rEgLbvJiE1hNlInlAtA-2WvhLfH4Xrw509YEd3qaml5MVVqNWiH0j2V6s8gCTr0unpXQ5PodytSql14mvU44_q1rixs11ZQ6XmFICK7-gD2HUZkxG2DOTuLD5a9Xr9PdlGQXIYRB5Id77i5lt34EcGXN45tfIIDuwWw2s8zozGndzS_dh50Y_j2dzedyDxGUxTWqLy46ET5WA";
-
+console.log('redirecting 2');
+const redirect = "https://tsabz.github.io/Algorthim-DJ-Part-1-/?code=AQAi8OmfGYck8rEgLbvJiE1hNlInlAtA-2WvhLfH4Xrw509YEd3qaml5MVVqNWiH0j2V6s8gCTr0unpXQ5PodytSql14mvU44_q1rixs11ZQ6XmFICK7-gD2HUZkxG2DOTuLD5a9Xr9PdlGQXIYRB5Id77i5lt34EcGXN45tfIIDuwWw2s8zozGndzS_dh50Y_j2dzedyDxGUxTWqLy46ET5WA";
+ $(location).attr("href", redirect);
 console.log('redirect done');
