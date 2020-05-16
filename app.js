f9e505b484e216c2ce3e26572265a07bd2a81807
Tonia Saba <teesabz@Mac-mini.local>
2020-05-16 19:01:38 -0400

    spotify authentication

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  console.log('access v.16');
+  console.log('access v.17');
 
   var getUrlParameter = function getUrlParameter(sParam) {
       var sPageURL = window.location.search.substring(1),
