702152689424980961b359302532a07ad6814564
Tonia Saba <teesabz@Mac-mini.local>
2020-05-16 16:19:50 -0400

    spotify authentication

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-
-
 $(document).ready(function(){
-console.log('access');
+  console.log('access');
+  const accessToken = getUrlParameter('code');
+  console.log(accessToken);
 
-const accessToken = $.param('code');
-console.log(accessToken);
+  var getUrlParameter = function getUrlParameter(sParam) {
+      var sPageURL = window.location.search.substring(1),
+          sURLVariables = sPageURL.split('&'),
+          sParameterName,
+          i;
+
+      for (i = 0; i < sURLVariables.length; i++) {
+          sParameterName = sURLVariables[i].split('=');
+
+          if (sParameterName[0] === sParam) {
+              return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+          }
+      }
+  };
 
 })
